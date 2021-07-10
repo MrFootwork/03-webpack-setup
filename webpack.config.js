d66e9c3f32c2d8e232508e433f74f6331ba573d7
Mr Footwork <pandau.ting@gmail.com>
2021-07-10 17:20:30 +0200

    Using source maps

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
 		index: './src/index.js',
 		print: './src/print.js',
 	},
+	devtool: 'inline-source-map',
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: 'Development',
@@ -17,5 +18,4 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 	},
-
 }
